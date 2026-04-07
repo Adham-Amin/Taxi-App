082b43645bb869afb68e1b07a9ae1550d9bca7b8
Adham-Amin <adhm.amin03@gmail.com>
2026-04-07 15:12:05 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CrazyApp());
+}
+
+class CrazyApp extends StatelessWidget {
+  const CrazyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
