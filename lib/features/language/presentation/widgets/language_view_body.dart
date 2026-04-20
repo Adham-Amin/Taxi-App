0636704eb6cf340f25c94f3813606e8bc3428afe
Adham-Amin <adhm.amin03@gmail.com>
2026-04-20 23:08:31 +0200

    Generate Local Keys

--- lib/features/language/presentation/widgets/language_view_body.dart
+++ lib/features/language/presentation/widgets/language_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:taxi_app/core/functions/extentions.dart';
+import 'package:taxi_app/core/lang/locale_keys.g.dart';
 import 'package:taxi_app/core/utils/app_styles.dart';
 import 'package:taxi_app/features/language/presentation/widgets/language_item.dart';
 
@@ -15,13 +16,13 @@ class LanguageViewBody extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           32.hs,
-          Text('select_language'.tr(), style: AppStyles.textMedium16),
+          Text(LocaleKeys.select_language.tr(), style: AppStyles.textMedium16),
           16.hs,
           LanguageItem(
             onTap: (_) {
               context.setLocale(const Locale('en'));
             },
-            text: 'english'.tr(),
+            text: LocaleKeys.english.tr(),
             value: context.locale.languageCode == 'en',
           ),
           16.hs,
@@ -29,7 +30,7 @@ class LanguageViewBody extends StatelessWidget {
             onTap: (_) {
               context.setLocale(const Locale('ar'));
             },
-            text: 'arabic'.tr(),
+            text: LocaleKeys.arabic.tr(),
             value: context.locale.languageCode == 'ar',
           ),
         ],
